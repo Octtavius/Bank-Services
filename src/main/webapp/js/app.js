24d061639e116699935518c0b85f173d36f0616b
Nando <nandox7@gmail.com>
2016-12-08 22:28:58 +0000

    Add new login page

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,4 +1,20 @@
-app = angular.module('bankwebpage', ['ui.utils.masks']);
+app = angular.module('bankwebpage', ['ui.utils.masks', 'ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+            .when("/", {
+                templateUrl: "pages/login.html"
+            })
+            .when("/main", {
+                templateUrl: "pages/main.html"
+            })
+            .when("/newaccount", {
+                templateUrl: "pages/newaccount.html"
+            });
+    $routeProvider.otherwise({redirectTo: '/'});
+});
+
+/*------------- Controllers --------------------------------------------------*/
 
 /* Test Controller */
 app.controller('Test', function ($scope, $http) {
@@ -27,6 +43,54 @@ app.controller('Balance', function ($scope, $http) {
 });
 
 
+// Define Login Form
+app.controller('LoginForm', function ($scope, $location, $http) {
+    $scope.formLoginData = {};
+    $scope.userDetails = {};
+    
+    $scope.processForm = function () {
+        console.log("Processing login form...");
+        $http({
+            method: 'POST',
+            url: 'http://localhost:3000/bankingonline/main/login',
+            data: $.param({
+                'accountId': $scope.formLoginData.accountId,
+                'password': $scope.formLoginData.password
+            }),
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+
+        }).then(function (response) {
+            console.log(response);
+
+            if (response.status !== 200) {
+                $scope.message = "ERROR: No account found!!!";
+            } else {
+
+                //console.log("Name: " + response.data.response);                
+                console.log("Login successfull...");
+                data = response.data;
+                
+                $scope.userDetails = {
+                    'accountId': '303',
+                    'firstName': data.firstName,
+                    'middleName': data.middleName,
+                    'lastName': data.lastName,
+                    'address': data.address,
+                    'email': data.email,
+                    'contactNumber': data.contactNumber
+                };
+
+                // Redirect page
+                $location.path("/SuccessPage");
+            }
+        });
+    };
+    
+});
+
+
 // Define Transfer Form
 app.controller('TransferForm', function ($scope, $http) {
     $scope.formTransferData = {};
@@ -49,7 +113,7 @@ app.controller('TransferForm', function ($scope, $http) {
             console.log(response);
 
             if (response.status !== 200) {
-                $scope.message = "ERROR: No account found!!!"
+                $scope.message = "ERROR: No account found!!!";
             } else {
                 //console.log("Name: " + response.data.response);
                 $scope.message = response.data.response;
@@ -77,7 +141,7 @@ app.controller('TransactionsTable', function ($scope, $http) {
             $scope.transactionList = response.data;
         });
     };
-    
+
     // Initial controller run
     $scope.loadData();
 
